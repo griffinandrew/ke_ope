bb64fa5154def82f608d511ad58bb683fc4f9a58
Griffin Heyrich <gheyrich@bu.edu>
2023-04-26 19:52:55 +0000

    adding python file to import libs and list them not verify yet

---
+++ .github/workflows/imort_test.py
@@ -0,0 +1,21 @@
+
+from IPython.core.display import HTML, Markdown, TextDisplayObject, Javascript
+from IPython.display import display, IFrame, Image
+import ipywidgets as widgets
+from ipywidgets import interact, fixed, Layout
+import os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select
+from notebook.notebookapp import list_running_servers
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import animation
+import numpy as np
+import pandas as pd
+import time
+import array
+
+
+
+for item in dir():
+    print(item)
+
+
