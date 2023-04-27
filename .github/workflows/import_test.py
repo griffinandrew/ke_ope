f8badb0a1a3fd16a731ad1a8614ab65a331bd65c
Griffin Heyrich <gheyrich@bu.edu>
2023-04-27 21:47:16 +0000

    trying without pywids

--- .github/workflows/import_test.py
+++ .github/workflows/import_test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 from IPython.core.display import HTML, Markdown, TextDisplayObject, Javascript
 from IPython.display import display, IFrame, Image
-import ipywidgets as widgets
-from ipywidgets import interact, fixed, Layout
+#import ipywidgets as widgets
+#from ipywidgets import interact, fixed, Layout
 import os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select
 from notebook.notebookapp import list_running_servers
 import matplotlib
