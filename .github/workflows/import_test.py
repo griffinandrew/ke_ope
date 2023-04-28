539a800f05d72cb44e43027cc0381b1851f4d063
Griffin Heyrich <gheyrich@bu.edu>
2023-04-28 19:01:02 +0000

    trying fixing

--- .github/workflows/import_test.py
+++ .github/workflows/import_test.py
@@ -5,18 +5,66 @@ try:
 except ImportError:
     raise ImportError('core.display modules failed to load')
 
-from IPython.display import display, IFrame, Image
-import ipywidgets as widgets
-from ipywidgets import interact, fixed, Layout
-import os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select
-from notebook.notebookapp import list_running_servers
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib import animation
-import numpy as np
-import pandas as pd
-import time
-import array
+try: 
+    from IPython.display import display, IFrame, Image
+except ImportError:
+    raise ImportError('display not loaded')
+
+try: 
+    import ipywidgets as widgets
+except ImportError
+    raise ImportError('ipywidgets fail')
+
+try:
+    from ipywidgets import interact, fixed, Layout
+except ImportError
+    raise ImportError('interact, fixed layout failed')
+
+try:
+    import os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select
+except ImportError
+    raise ImportError(" os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select failed")
+
+try: 
+    from notebook.notebookapp import list_running_servers
+except ImportError
+    raise ImportError("notebook.notebookapp import list_running_servers failed")
+
+try:
+    import matplotlib
+except ImportError
+    raise ImportError("import matplotlib failed")
+
+try:
+    import matplotlib.pyplot as plt
+except ImportError
+    raise ImportError("import matplotlib.pyplot as plt failed")
+
+try:
+    from matplotlib import animation
+except ImportError
+    raise ImportError("from matplotlib import animation failed")
+
+try:
+    import numpy as np
+except ImportError
+    raise ImportError("import numpy as np failed")
+
+try:
+    import pandas as pd
+except ImportError
+    raise ImportError(" import pandas as pd failed")
+
+try:
+    import time
+except ImportError
+    raise ImportError("import time failed")
+
+try:
+    import array
+except ImportError
+    raise ImportError("import array failed")
+
 
 def main():
 
