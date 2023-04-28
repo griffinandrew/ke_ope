ad240de983579286cc26754e65d6829a790e037d
Griffin Heyrich <gheyrich@bu.edu>
2023-04-28 16:13:36 +0000

    fixing script?

--- .github/workflows/import_test.py
+++ .github/workflows/import_test.py
@@ -1,28 +1,36 @@
 #!/usr/bin/env python
-print(1)
-from IPython.core.display import HTML, Markdown, TextDisplayObject, Javascript
-print(2)
-from IPython.display import display, IFrame, Image
-print(3)
-import ipywidgets as widgets
-print(4)
-from ipywidgets import interact, fixed, Layout
-print(5)
-import os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select
-print(6)
-from notebook.notebookapp import list_running_servers
-print(7)
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib import animation
-import numpy as np
-import pandas as pd
-import time
-import array
+
+
+def main():
+
+    print(1)
+    from IPython.core.display import HTML, Markdown, TextDisplayObject, Javascript
+    print(2
+    from IPython.display import display, IFrame, Image
+    print(3)
+    import ipywidgets as widgets
+    print(4)
+    from ipywidgets import interact, fixed, Layout
+    print(5)
+    import os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select
+    print(6)
+    from notebook.notebookapp import list_running_servers
+    print(7)
+    import matplotlib
+    import matplotlib.pyplot as plt
+    from matplotlib import animation
+    import numpy as np
+    import pandas as pd
+    import time
+    import array
 
 
 
-for item in dir():
-    print(item)
+    for item in dir():
+        print(item)
 
 
+
+
+if __name__ == "__main__":
+    main()
