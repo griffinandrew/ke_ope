d4eaf1f390f86242614dd1e40abd0a79bd759f05
Griffin Heyrich <gheyrich@bu.edu>
2023-04-28 16:00:48 +0000

    trying to find error

--- workflows/import_test.py
+++ workflows/import_test.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
+print(1)
 from IPython.core.display import HTML, Markdown, TextDisplayObject, Javascript
+print(2)
 from IPython.display import display, IFrame, Image
-#import ipywidgets as widgets
-#from ipywidgets import interact, fixed, Layout
+print(3)
+import ipywidgets as widgets
+print(4)
+from ipywidgets import interact, fixed, Layout
+print(5)
 import os, requests, pty, re, subprocess, struct, sys, fcntl, termios, select
+print(6)
 from notebook.notebookapp import list_running_servers
+print(7)
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import animation
